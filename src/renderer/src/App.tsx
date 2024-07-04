98a88c53d6df858cc4ffa51de0eb2bdc6d65470e
Vinit Shirbhate <vinit.shirbhate23@vit.edu>
2024-07-04 23:29:57 +0530

    elctron.js initial setup with tailwind

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <span className="text-red-500 text-4xl font-bold">Hello world!</span>
+    </div>
+  )
+}
+
+export default App
